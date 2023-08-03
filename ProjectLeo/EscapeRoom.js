1132e43521d53d5669aee1477f29c635c16918d1
privatebench <moodychair24@gmail.com>
2023-08-03 19:46:32 +0800

    Test
    Uploading para maload yunng image sa generator tas makuha ung coords yup

---
+++ ProjectLeo/EscapeRoom.js
@@ -0,0 +1,7 @@
+// For Scene 1
+
+function Scene1_Change() {
+    alert("nacall ung function");
+    //document.getElementById("Panel1").src = "Panel 1 (" + item + ").png";
+    //document.getElementById("Panel1").usermap = "#Scene1_" + item;
+}
